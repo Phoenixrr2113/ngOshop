d3f505388dd2a4c20f8e327e975a61eadd3d2ba3
Randy Wilson <phoenixrr2113@gmail.com>
2019-06-11 23:16:55 -0400

    created some components and installed packages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBy98SWcFIAl44A3SlXSEJYSYqTsoZB4Qg',
+    authDomain: 'shopapp-f2098.firebaseapp.com',
+    databaseURL: 'https://shopapp-f2098.firebaseio.com',
+    projectId: 'shopapp-f2098',
+    storageBucket: 'shopapp-f2098.appspot.com',
+    messagingSenderId: '1006583943379',
+    appId: '1:1006583943379:web:05643c333de69935',
+    // Initialize Firebase
+    // firebase.initializeApp(firebaseConfig)
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBy98SWcFIAl44A3SlXSEJYSYqTsoZB4Qg',
+    authDomain: 'shopapp-f2098.firebaseapp.com',
+    databaseURL: 'https://shopapp-f2098.firebaseio.com',
+    projectId: 'shopapp-f2098',
+    storageBucket: 'shopapp-f2098.appspot.com',
+    messagingSenderId: '1006583943379',
+    appId: '1:1006583943379:web:05643c333de69935',
+    // Initialize Firebase
+    // firebase.initializeApp(firebaseConfig)
+  },
 };
 
 /*
